ae80ce916db56a352f8543aacef621f8b3601a36
anna borzenkowa <crownedjocker@gmail.com>
2023-05-18 13:12:30 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 28745;
+        int rubles = 20;
+
+        System.out.println((amount / rubles) + " миль ");
+    }
+}
